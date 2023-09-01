0c726b798353cbda86e8608c8bc6207cc60151fa
Tsuyoshi Ogawa <t11a@Tsuyoshis-MacBook-Pro.local>
2023-09-01 22:40:11 +0900

    add test for is_reaction_message

---
+++ tests/test_is_reaction_message.py
@@ -0,0 +1,18 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import unittest
+from lambda_function.handler import is_reaction_message
+
+class TestIsReactionMessage(unittest.TestCase):
+    def test_valid_reaction_messages(self):
+        self.assertTrue(is_reaction_message("username++ ありがとう。"))
+        self.assertTrue(is_reaction_message("username++ username++ username++ ありがとう。"))
+        self.assertTrue(is_reaction_message("username++"))
+
+    def test_invalid_reaction_messages(self):
+        self.assertFalse(is_reaction_message("ただのメッセージ"))
+        self.assertFalse(is_reaction_message("username+-"))
+        self.assertFalse(is_reaction_message("++username"))

9c472f85b7440e7e2a352772e66f69b3810e72a8
Tsuyoshi Ogawa <t11a@Tsuyoshis-MacBook-Pro.local>
2023-09-02 16:16:23 +0900

    add skip mark

--- tests/test_post_message.py
+++ tests/test_post_message.py
@@ -7,10 +7,12 @@ import unittest
 from lambda_function.handler import post_message
 from secret_config import SLACK_TOKEN, channel_id
 import random
+import pytest
 
 # This test actually executes the Slack API, so it's closer to an Integration test.
 class TestPostMessage(unittest.TestCase):
     
+    @pytest.mark.skip(reason="The test is marked because it actually posts to a Slack channel.")
     def test_valid_post_message(self):
         random_number = random.randint(0, 100)
         text = "username: {}".format(random_number)

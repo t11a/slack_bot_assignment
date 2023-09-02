58f960685420c9d9b9e0eea1cb19830844c32351
Tsuyoshi Ogawa <t11a@Tsuyoshis-MacBook-Pro.local>
2023-09-02 23:04:58 +0900

    update extract_data

--- tests/test_extract_data.py
+++ tests/test_extract_data.py
@@ -7,47 +7,35 @@ import unittest
 from lambda_function.handler import extract_data
 
 class TestExtractData(unittest.TestCase):
-    def test_single_username_jp(self):
-        text = "username++ ありがとう。"
-        count, message = extract_data(text)
-        self.assertEqual(count, 1)
-        self.assertEqual(message, "ありがとう。")
+    def test_extract_single_username(self):
+        text = "alice++"
+        expected = {'alice': 1}
+        result = extract_data(text)
+        self.assertEqual(expected, result)
 
-    def test_single_username_en(self):
-        text = "username++ Thank you:)"
-        count, message = extract_data(text)
-        self.assertEqual(count, 1)
-        self.assertEqual(message, "Thank you:)")
-    """
-    def test_username_include_period(self):
-        text = "user.name++ Thank you:)"
-        count, message = extract_data(text)
-        self.assertEqual(count, 1)
-        self.assertEqual(message, "Thank you:)")
-    """
-    def test_multiple_usernames(self):
-        text = "username++ username++ username++ ありがとう。"
-        count, message = extract_data(text)
-        self.assertEqual(count, 3)
-        self.assertEqual(message, "ありがとう。")
+    def test_extract_multiple_different_usernames(self):
+        text = "alice++ bob++"
+        expected = {'alice': 1, 'bob': 1}
+        result = extract_data(text)
+        self.assertEqual(expected, result)
 
-    def test_no_message(self):
-        text = "username++ username++ username++"
-        count, message = extract_data(text)
-        self.assertEqual(count, 3)
-        self.assertIsNone(message)
+    def test_extract_multiple_same_usernames(self):
+        text = "alice++ alice++"
+        expected = {'alice': 2}
+        result = extract_data(text)
+        self.assertEqual(expected, result)
 
-    def test_double_spaces_before_message(self):
-        text = "username++ username++ username++  ありがとう。"
-        count, message = extract_data(text)
-        self.assertEqual(count, 3)
-        self.assertEqual(message, "ありがとう。")
+    def test_extract_mixed_usernames(self):
+        text = "alice++ bob++ alice++"
+        expected = {'alice': 2, 'bob': 1}
+        result = extract_data(text)
+        self.assertEqual(expected, result)
 
-    def test_multibyte_space_before_message(self):
-        text = "username++　ありがとう。"
-        count, message = extract_data(text)
-        self.assertEqual(count, 1)
-        self.assertEqual(message, "ありがとう。")
+    def test_no_usernames(self):
+        text = "Hello World!"
+        expected = {}
+        result = extract_data(text)
+        self.assertEqual(expected, result)
 
 if __name__ == '__main__':
     unittest.main()

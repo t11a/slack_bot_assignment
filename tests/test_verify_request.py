1421d2a3daeedf0a0c5132173cd693f1cf1fad75
Tsuyoshi Ogawa <t11a@Tsuyoshis-MacBook-Pro.local>
2023-09-05 21:41:39 +0900

    use env variable

--- tests/test_verify_request.py
+++ tests/test_verify_request.py
@@ -5,14 +5,14 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import unittest
 from lambda_function.handler import verify_request
-from secret_config import valid_event, invalid_event, SLACK_SIGNING_SECRET
+from secret_config import valid_event, invalid_event
 
 class TestVerifyRequest(unittest.TestCase):
     def test_valid_request(self):
-        self.assertTrue(verify_request(valid_event, SLACK_SIGNING_SECRET))
+        self.assertTrue(verify_request(valid_event, os.environ['SLACK_SIGNING_SECRET']))
 
     def test_invalid_request(self):
-        self.assertFalse(verify_request(invalid_event, SLACK_SIGNING_SECRET))
+        self.assertFalse(verify_request(invalid_event, os.environ['SLACK_SIGNING_SECRET']))
         #with self.assertRaises(ValueError):
         #    verify_request(invalid_event)
 

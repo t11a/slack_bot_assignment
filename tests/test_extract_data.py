403c71fe3351c6c5fbe161ace71ab2846148a398
Tsuyoshi Ogawa <t11a@Tsuyoshis-MacBook-Pro.local>
2023-09-18 19:41:13 +0900

    update text for extract_data method

--- tests/test_extract_data.py
+++ tests/test_extract_data.py
@@ -8,25 +8,25 @@ from lambda_function.handler import extract_data
 
 class TestExtractData(unittest.TestCase):
     def test_extract_single_username(self):
-        text = "alice++"
+        text = "alice++ Thanks!"
         expected = {'alice': 1}
         result = extract_data(text)
         self.assertEqual(expected, result)
 
     def test_extract_multiple_different_usernames(self):
-        text = "alice++ bob++"
+        text = "alice++ bob++ Thanks!"
         expected = {'alice': 1, 'bob': 1}
         result = extract_data(text)
         self.assertEqual(expected, result)
 
     def test_extract_multiple_same_usernames(self):
-        text = "alice++ alice++"
+        text = "alice++ alice++ Thanks!"
         expected = {'alice': 2}
         result = extract_data(text)
         self.assertEqual(expected, result)
 
     def test_extract_mixed_usernames(self):
-        text = "alice++ bob++ alice++"
+        text = "alice++ bob++ alice++ Thanks!"
         expected = {'alice': 2, 'bob': 1}
         result = extract_data(text)
         self.assertEqual(expected, result)
